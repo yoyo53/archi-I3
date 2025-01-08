33eedf151eed07ad0822a1032acc88d594d8d200
Kurnoth <arnaud.kohler@bbox.fr>
2025-01-08 13:30:53 -0100

    Initialization

---
+++ dbservice/src/main/java/database/service/dbservice/DbserviceApplication.java
@@ -0,0 +1,17 @@
+package database.service.dbservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@SpringBootApplication
+public class DbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbserviceApplication.class, args);
+
+	}
+
+}
---
+++ dbservice/src/test/java/database/service/dbservice/DbserviceApplicationTests.java
@@ -0,0 +1,5 @@
+package database.service.dbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
